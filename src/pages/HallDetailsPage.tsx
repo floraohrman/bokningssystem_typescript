383acd26b761864b5f9901f9460bd62ad0ece1ff
Eiswohld <eiswohld@hotmail.com>
2026-09-02 13:05:07 +0200

    struktur för routing

---
+++ src/pages/HallDetailsPage.tsx
@@ -0,0 +1,12 @@
+// kommer innehålla dynamisk route
+
+function HallDetailsPage() {
+  return (
+    <main>
+      <h1>Hall</h1>
+      <p>Här kommer information om den valda hallen att visas </p>
+    </main>
+  );
+}
+
+export default HallDetailsPage;
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,11 @@
+// startsida
+function HomePage() {
+  return (
+    <main>
+      <h1>Välkommen till BALLERS</h1>
+      <p>Boka din hall för fotboll, handboll eller innebandy</p>
+    </main>
+  );
+}
+
+export default HomePage;
