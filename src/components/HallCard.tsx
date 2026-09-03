4fdfa15d9b40a3011aac27875777a7c157d38209
teddylindstrom <teddy.lindstrom.work@gmail.com>
2026-09-03 15:17:16 +0200

    skapat hallcard för bokning av hallar

---
+++ src/components/HallCard.tsx
@@ -0,0 +1,21 @@
+interface Hall {
+  name: string;
+  price: number;
+}
+
+interface HallCardProps {
+  hall: Hall;
+}
+
+function HallCard({ hall }: HallCardProps) {
+  return (
+    <article>
+      <h2>{hall.name}</h2>
+      <p>Pris: {hall.price} kr</p>
+
+      <button>Visa hall</button>
+    </article>
+  );
+}
+
+export default HallCard;
